bcf3400b8fa243c8cc60b72a2af003da6209efc5
Humberto Hernandez <hamburger45@gmail.com>
2020-03-09 23:46:30 -0700

    added emp img

--- src/components/EmployeeCard/EmployeeCard.js
+++ src/components/EmployeeCard/EmployeeCard.js
@@ -5,8 +5,14 @@ export default class EmployeeCard extends Component {
 
   render() {
     return (
-      <div>
-        <h1>test</h1>
+      <div className='employee-card'>
+        <div className='image'><img src="https://images.unsplash.com/photo-1521146764736-56c929d59c83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="Employee Image"></img></div>
+        <div className='info-card'>
+          <div className='info-data'></div>
+          <div className='info-data'></div>
+          <div className='info-data'></div>
+          <div className='info-data'></div>
+        </div>
       </div>
     )
   }
---
+++ src/components/EmployeeCard/data.js
@@ -0,0 +1,8 @@
+// https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80
+// @juricakoletic
+
+// https://images.unsplash.com/photo-1474176857210-7287d38d27c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80
+//@hasseykhan 
+
+// https://images.unsplash.com/photo-1521146764736-56c929d59c83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80
+// @hasseykhan
